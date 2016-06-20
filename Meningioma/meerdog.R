eaa5b1a25fba0278589e78c07ad04dc05f3ca271
ngreenwald <noahfgreenwald@gmail.com>
2016-06-20 10:32:17 -0400

    it's growing

--- Meningioma/meerdog.R
+++ Meningioma/meerdog.R
@@ -1,31 +1,50 @@
 ## Simplified Meerkat style rearrangement caller
 
-
-## Psuedo code
+rtrack <- import("C:/Users/Noah/OneDrive/Work/Coding/R/dbs/psuedogenes.bed", format = "BED")
+## read in regions of genome with tandem repeats
+vntr <- readRDS("C:/Users/Noah/OneDrive/Work/Coding/R/dbs/gr.repeatMasker.rds")
+## read in regions with transposable elements
+te <- readRDS("C:/Users/Noah/OneDrive/Work/Coding/R/dbs/tubio_l1.rds")
 
 for (i in 1:nrow(events.matrix)){
-    ## r track layer to import bed files
     
-    ## They first check if rearrangment location is covered by transposable element
-    ## would best way to do this be with one of the tracks in GISTIC?
-    if (events.matrix[i]$base.pair.position %in% TE.track){
-        events.matrix[i]$rearrangment.classification <- "TE"
+    # sets variables for current rearrangement
+    pos1 <- events.matrix[i, "pos1"]
+    chr1 <- events.matrix[i, "chr1"]
+    pos2 <- events.matrix[i, "pos2"]
+    chr2 <- events.matrix[i, "chr2"]
     
-    ## They next check if overlaps with Variable Number of Tandem Repeats region   
-    }else if (events.matrix[i]$base.pair.position %in% VNTR.track){
-        events.matrix[i]$rearrangement.classification <- "VNTR"
+    ## First check if either end of rearrangment is located within transposable element
+    ## buffer this one. 
+    te.candidates <- rtrack[rtrack@ranges@start < pos1 & (rtrack@ranges@width + rtrack@ranges@start) > pos1 & rtrack@seqnames == chr1]
+    te.candidates2 <- rtrack[rtrack@ranges@start < pos2 & (rtrack@ranges@width + rtrack@ranges@start) > pos2 & rtrack@seqnames == chr2]
+    
+    if (length(start(ranges(te.candidates))) > 0){
+        events.matrix[i]$meerdog <- "TE"
         
+    }else if (length(start(ranges(te.candidates2))) > 0){
+        events.matrix[i]$meerdog <- "TE"
+        
+    }
+    ## Next check if overlaps with Variable Number of Tandem Repeats region   
+    vntr.candidates <- rtrack[rtrack@ranges@start < pos1 & (rtrack@ranges@width + rtrack@ranges@start) > pos1 & rtrack@seqnames == chr1]
+    vntr.candidates2 <- rtrack[rtrack@ranges@start < pos2 & (rtrack@ranges@width + rtrack@ranges@start) > pos2 & rtrack@seqnames == chr2]
+    
+    if (length(start(ranges(vntr.candidates))) > 0){
+        events.matrix[i]$meerdog <- "VNTR"
+        
+    }else if (length(start(ranges(vntr.candidates2))) > 0){
+        events.matrix[i]$meerdog <- "VNTR"
+        
+     
     ## After first two options exhausted, remaining insertions get binned as NA
-    ## Would SVtype be where i find categorization as insertion or deletion? Or should manual step be added here? Seems like it
-    ## would be pretty straight forward to classify simple events as insertion or deletion based on DNA orientation info that plots
-    ## are putting out
     }else if (events.matrix[i]$event.type == "insertion"){
         events.matrix[i]$rearrangement.classification <- "NA"
     
     ## They next check if deletion breakpoints have insertion at the same location. This is just your insertion category
-    }else if (events.matrix[i]insertion == T){
+    }else if (events.matrix[i]$insertion == T){
         
-        ## If deletion breakpoint is > 10 bp vs < 10 bp, diferent calls
+        ## If insertion is > 10 bp vs < 10 bp, diferent calls
         if (events.matrix[i]$breakpoint.length > 10){
             events.matrix[i]$rearrangement.classification <- "MMBIR"
         }else{
@@ -44,5 +63,9 @@ for (i in 1:nrow(events.matrix)){
     }else{
         events.matrix[i]$rearrangment.classification <- "NHEJ"
     }
-        
+    
+}else{
+    events.matrix$meerdog[i] <- "not evaluated"
+
+            
 }
