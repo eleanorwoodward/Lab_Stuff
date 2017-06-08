bbd41ac26cc64a186fff29a1c44dc39fd8a5424a
ngreenwald <noahfgreenwald@gmail.com>
2017-06-07 20:02:27 -0400

    changed NMF inputs to be defind by 20% cutoff

--- Pan_CNS_Oncopanel/Clustering.R
+++ Pan_CNS_Oncopanel/Clustering.R
@@ -72,6 +72,8 @@ theme(legend.position = "none", axis.ticks = element_blank(), axis.text.x = elem
 
 ## use NMF to cluster
 ## adapted from: https://cran.r-project.org/web/packages/NMF/vignettes/heatmaps.pdf and 
+
+## first generate combined alterations list
 alterations <- all.mutations.tier1.4[, c("SAMPLE_ACCESSION_NBR", "BEST_EFF_GENE", "variant_classification")]
 alterations.cnv <- all.cnv.high[, c("SAMPLE_ACCESSION_NBR", "GENE", "CNV_TYPE_CD")]
 colnames(alterations.cnv) <- colnames(alterations)
@@ -80,103 +82,128 @@ alterations <- rbind(alterations, alterations.cnv, alterations.svs)
 alterations$BEST_EFF_GENE[alterations$BEST_EFF_GENE %in% c("CDKN2A", "CDKN2B")] <- "CDKN2A/B"
 alterations$BEST_EFF_GENE[alterations$BEST_EFF_GENE == "MYCN"] <- "MYC"
 alterations$variant_classification <- "altered"
-alterations.trimmed <- alterations[alterations$BEST_EFF_GENE %in% c("TP53", "IDH1", "NF2", "MYC", "KMT2D", "PTPRD", "PRKDC", "KRAS", 
-                                                                    "PTCH1", "PTEN", "EGFR", "ATRX", "CDKNA/B", "NF1", "SMARC4A"), ]
+
+## next restrict to only those present in at least 20% of tumor types that have at least 100 samples each
+trimmed <- c()
+for (i in 1:length(pathologies)){
+    if (length(pathologies[[i]]) > 100){
+        current.alterations <- alterations[alterations$SAMPLE_ACCESSION_NBR %in% pathologies[[i]], ]
+        candidates <- sort(table(current.alterations$BEST_EFF_GENE), decreasing = TRUE)
+        finalists <- names(candidates[candidates > floor(length(pathologies[[i]]) / 5)])
+        trimmed <- c(trimmed, finalists)
+    }
+}
+
+trimmed <- unique(trimmed)
+
+## figure out which samples have no mutations
+alterations.trimmed <- alterations[alterations$BEST_EFF_GENE %in% trimmed, ]
 names <- unique(master.sheet$SAMPLE_ACCESSION_NBR)
-quiet.idx <- sapply(names, function(x){if(sum(alterations.trimmed$SAMPLE_ACCESSION_NBR == x) == 0){return(TRUE)}else{return(FALSE)}}, USE.NAMES = FALSE)
-quiet.names <- names[quiet.idx]
+quiet.names <- names[!(names %in% alterations.trimmed$SAMPLE_ACCESSION_NBR)]
 quiet.samples <- cbind(quiet.names, "quiet.mut", "altered")
 colnames(quiet.samples) <- colnames(alterations.trimmed)
 alterations.trimmed <- rbind(alterations.trimmed, quiet.samples)
 
-## feature selection time! consolidate arm-level CNVs
-clustering.cnvs <- all.cnvs.broad
+## Add meta features
+clustering.cnvs.gain <- all.cnvs.broad
+clustering.cnvs.gain[clustering.cnvs.gain < 0] <- 0
+clustering.cnvs.loss <- all.cnvs.broad
+clustering.cnvs.loss[clustering.cnvs.loss > 0] <- 0
+clustering.cnvs.loss <- abs(clustering.cnvs.loss)
+rownames(clustering.cnvs.gain) <- paste(unique(gene.to.arm$ID), "_gain", sep = "")
+rownames(clustering.cnvs.loss) <- paste(unique(gene.to.arm$ID), "_loss", sep = "")
+
+clustering.cnvs <- rbind(clustering.cnvs.gain, clustering.cnvs.loss)
 clustering.cnvs["count", ] <- as.numeric(apply(clustering.cnvs, 2, function(x){sum(x != 0)}))
-clustering.cnvs["1p.19q", clustering.cnvs["1p", ] == -1 & clustering.cnvs["19q", ] == -1] <- 1
-clustering.cnvs["1p.19q", is.na(clustering.cnvs["1p.19q", ])] <- 0
-clustering.cnvs["7pq", clustering.cnvs["7p", ] == 1 & clustering.cnvs["7q", ] == 1] <- 1
-clustering.cnvs["7pq", is.na(clustering.cnvs["7pq", ])] <- 0
-clustering.cnvs["disrupted", clustering.cnvs["count", ] > 5 ] <- 1
-clustering.cnvs["disrupted", is.na(clustering.cnvs["disrupted", ])] <- 0
-clustering.cnvs["quiet", clustering.cnvs["count", ] == 0] <- 1
-clustering.cnvs["quiet", is.na(clustering.cnvs["quiet", ])] <- 0
+quantile(as.numeric(clustering.cnvs[69, ]))
+clustering.cnvs["cnv.disrupted", clustering.cnvs["count", ] > 6 ] <- 1
+clustering.cnvs["cnv.disrupted", is.na(clustering.cnvs["cnv.disrupted", ])] <- 0
+clustering.cnvs["cnv.quiet", clustering.cnvs["count", ] == 0] <- 1
+clustering.cnvs["cnv.quiet", is.na(clustering.cnvs["cnv.quiet", ])] <- 0
+clustering.cnvs["cnv.middle", clustering.cnvs["count", ] %in% 1:5] <- 1
+clustering.cnvs["cnv.middle", is.na(clustering.cnvs["cnv.middle", ])] <- 0
+clustering.cnvs <- clustering.cnvs[-69, ]
+
+## same as above, figure out which features are present in at least 20% of cases
+trimmed.broad <- c()
+for (i in 1:length(pathologies)){
+    if (length(pathologies[[i]]) > 100){
+        current.samples <- clustering.cnvs[, names.fixed %in% pathologies[[i]]]
+        candidates <- rowSums(current.samples)
+        finalists <- rownames(current.samples)[candidates > floor(length(pathologies[[i]]) / 5)]
+        trimmed.broad <- c(trimmed.broad, finalists)
+    }
+}
+
+trimmed.broad <- unique(trimmed.broad)
+
+clustering.cnvs.trimmed <- clustering.cnvs[trimmed.broad, ]
 
 
-## isolate only direction we're interested in for specific chromosomes
-clustering.cnvs["1p", clustering.cnvs["1p", ] == 1] <- 0
-clustering.cnvs["1p", clustering.cnvs["1p", ] == -1] <- 1
+clustering.clinical <- master.sheet[, c("SAMPLE_ACCESSION_NBR", "Cancer_Type_Broad", "Cancer_Type_Specific")]
 
-clustering.cnvs["10q", clustering.cnvs["10q", ] == 1] <- 0
-clustering.cnvs["10q", clustering.cnvs["10q", ] == -1] <- 1
-
-clustering.cnvs["17p", clustering.cnvs["17p", ] == 1] <- 0
-clustering.cnvs["17p", clustering.cnvs["17p", ] == -1] <- 1
-
-clustering.cnvs["22q", clustering.cnvs["22q", ] == 1] <- 0
-clustering.cnvs["22q", clustering.cnvs["22q", ] == -1] <- 1
-clustering.cnvs.trimmed <- clustering.cnvs[c("1p","7pq", "10q", "17p", "1p.19q", "22q", "disrupted", "quiet"), ]
-
-
-clustering.clinical <- master.sheet[, c("SAMPLE_ACCESSION_NBR", "Cancer_Type_Broad", "exclude")]
-clustering.clinical$Cancer_Type_Broad[clustering.clinical$exclude == "bch"] <- "pediatric"
-
-clustering.input <- PlotComut(
+clustering.input.cnvs <- PlotComut(
     mut.maf1 = alterations.trimmed,
     broad.cnv.maf = clustering.cnvs.trimmed,
     samples = master.sheet$SAMPLE_ACCESSION_NBR,
     input.samples = "SAMPLE_ACCESSION_NBR", 
-    input.genes = "BEST_EFF_GENE", input.mutations = "variant_classification", gene.cutoff = 60, 
+    input.genes = "BEST_EFF_GENE", input.mutations = "variant_classification", 
     return.matrix = TRUE,
-    dimensions = c(18,10), 
+    dimensions = c(18,10),
+    #manual.order = c("quiet.mut", "cnv.quiet", "cnv.disrupted"), 
     col.vector = c("frameshift_indel" = "red", "missense" ="red" , "nonsense" = "red", "splice_site" = "red", "stop_codon" = "red", "in_frame_indel" = "red",
                    "other" = "red", "arm-level gain" = "red", "arm-level loss" = "red", "2DEL" = "red", "HA" = "red", "indel" = "red", "rearrangement" = "red",
                    "Glioblastoma" = "black", "AnaplasticAstro" = "green", "Angiocentric" = "orange", "Astro" = "green", "altered" = "red", 
                    "DiffuseAstro" = "green", "Oligo" = "yellow", "OligoAstro" = "yellow", "PilocyticAstro" = "green", 
                    "0" = "gray90", "1" = "gray70", "2" = "gray50", "3" = "gray25", "4" = "gray1")
-    ,phenotypes = clustering.clinical[, -3]
+    ,phenotypes = clustering.clinical
 )
 
 
-## make fake data for testing
-samples <- c(rep("A", 40), rep("B", 40), rep("C", 40))
-row1 <- c(rbinom(40, 1, .8), rbinom(40, 1, .5), rbinom(40, 1, .2))
-row2 <- c(rbinom(40, 1, .9), rbinom(40, 1, .5), rbinom(40, 1, .1))
-row3 <- c(rbinom(40, 1, .9), rbinom(40, 1, .1), rbinom(40, 1, .9))
-row4 <- c(rbinom(40, 1, .1), rbinom(40, 1, .9), rbinom(40, 1, .4))
-row5 <- c(rbinom(40, 1, .9), rbinom(40, 1, .3), rbinom(40, 1, .9))
-row6 <- c(rbinom(40, 1, .1), rbinom(40, 1, .1), rbinom(40, 1, .9))
-test.matrix <- rbind(row1, row2, row3, row4, row5, row6)
-test.matrix <- test.matrix[, colSums(test.matrix) != 0]
-
-test.output <- nmf(test.matrix, 4)
-aheatmap(test.matrix, annCol = samples, annColors = list(c("red", "blue", "green")))
-coefmap(minfit(test.output), annCol = samples, Colv = "basis", annColors = list(c("red", "orange", "purple", "green", "black", "blue")))
-
-## change to non-negative matrix, features as rows and samples as columns (still not sure why not reversed??)
-nmf.input <- reshape(clustering.input, idvar = "samples", timevar = "genes", direction = "wide")
+## change to non-negative matrix, features as rows and samples as columns
+clustering.input.gliomas <- clustering.input[clustering.input$samples %in% master.sheet$SAMPLE_ACCESSION_NBR[master.sheet$Cancer_Type_Broad == "Glioma"], ]
+nmf.input <- reshape(clustering.input.gliomas, idvar = "samples", timevar = "genes", direction = "wide")
 
 nmf.input[nmf.input == "wt"] <- 0
 nmf.input[, -1][nmf.input[, -1] != 0] <- 1
 nmf.input <- t(nmf.input)
 colnames(nmf.input) <- nmf.input[1, ]
 nmf.input <- nmf.input[-1, ]
+row.labels <- rownames(nmf.input)
 nmf.input <- apply(nmf.input, 2, as.numeric)
+rownames(nmf.input) <- gsub("(mutations.)(*)", "\\2", row.labels)
 
 ## remove empty columns
+## figure out number of covariates incldued in clinical clustering, remove that many from end
+end <- nrow(nmf.input)
+remove <- ncol(clustering.clinical) - 2
+start <- end - remove
+nmf.input <- nmf.input[-c(start:end), ]
 nmf.input <- nmf.input[, colSums(nmf.input) != 0]
-nmf.input <- nmf.input[-23, ]
 
 
 column.annotations <- master.sheet
 column.annotations <- column.annotations[column.annotations$SAMPLE_ACCESSION_NBR %in% colnames(nmf.input), ]
 column.annotations$Cancer_Type_Broaddd <- column.annotations$Cancer_Type_Broad
+column.annotations$Cancer_Type_Glioma[column.annotations$Cancer_Type_Broad == "Glioma"] <- 
+    column.annotations$Cancer_Type_Specific[column.annotations$Cancer_Type_Broad == "Glioma"]
+column.annotations$Cancer_Type_Glioma[!(column.annotations$Cancer_Type_Glioma %in% 
+                                            c("Astro", "Oligo", "Glioblastoma", "OligoAstro", "AnaplasticAstro", "DiffuseAstro", "Pediatric"))] <- "glioma_other"
+column.annotations$Cancer_Type_Glioma[column.annotations$Cancer_Type_Glioma %in% c("AnaplasticAstro", "DiffuseAstro")] <- "Astro"
+column.annotations$Cancer_Type_Glioma[column.annotations$Cancer_Type_Glioma %in% c("Oligo", "OligoAstro")] <- "Oligo"
 column.annotations$Cancer_Type_Broaddd[column.annotations$Cancer_Type_Broaddd %in% c("Craniopharyngioma", "Paraganglioma",  "Pineal_parenchymal_tumor", "Chondrosarcoma",
                                                                                      "Schwannoma", "Chordoma", "Schwannoma check", "Neuroblastoma", "Lymphoma",
                                                                                      "Medulloblastoma", "Pituitary_other", "Ependymoma", "Paraganglioglioma", "SFT")] <- "other"
+column.annotations$Cancer_Type_Broaddd_Glioma <- column.annotations$Cancer_Type_Broaddd
+column.annotations$Cancer_Type_Broaddd_Glioma[column.annotations$Cancer_Type_Broad == "Glioma"] <- column.annotations$Cancer_Type_Glioma[column.annotations$Cancer_Type_Broad == "Glioma"]
+
+column.annotations$Cancer_Type_Broad_Glioma <- column.annotations$Cancer_Type_Broad
+column.annotations$Cancer_Type_Broad_Glioma[column.annotations$Cancer_Type_Broad == "Glioma"] <- column.annotations$Cancer_Type_Glioma[column.annotations$Cancer_Type_Broad == "Glioma"]
+column.annotations$Cancer_Type_Glioma_Grade <- paste(column.annotations$Cancer_Type_Glioma, column.annotations$Grade, sep = "_")
+column.annotations$Cancer_Type_Glioma_Grade[!(column.annotations$Cancer_Type_Glioma_Grade %in% c("Astro_2", "Astro_3", "Glioblastoma_4", 
+                                                                                                 "glioma_other_", "Oligo_2", "Oligo_3", "Pediatric_"))] <- "other"
 ## re order based on nmf order
 column.annotations <- column.annotations[order(match(column.annotations$SAMPLE_ACCESSION_NBR, colnames(nmf.input))), ]
-## order columns by consensus classes defined from given number of iterations with set k value
-aheatmap(nmf.input, annCol = column.annotations$Cancer_Type_Broaddd, annColors = list(c("red", "yellow", "blue", "green", "purple", "orange")))
 
 
 ## if multiple runs
@@ -184,23 +211,66 @@ nmf.output.4 <- nmf(nmf.input, 2, nrun = 2)
 coefmap(nmf.output.4, Colv = "consensus")
 
 ## if single run
-nmf.output.3 <- nmf(nmf.input, 3)
-pdf("../Analysis/NMF K2 heatmap.pdf")
-coefmap(minfit(nmf.output.2), annCol = column.annotations$Cancer_Type_Broaddd, annColors = list(c("red", "yellow", "blue", "green", "purple", "orange")), Colv = "basis")
+nmf.output.glioma.4 <- nmf(nmf.input, 4, seed = 123456789)
+tiff("../Analysis/NMF Glioma K4 heatmap.tiff", width = 1000, height = 1000)
+pdf("../Analysis/NMF K4 heatmap.pdf")
+coefmap(minfit(nmf.output.glioma.4), annCol = column.annotations$Cancer_Type_Glioma, annColors = list(c("red", "yellow", "blue", "green4", "purple", "orange")), Colv = "basis")
 dev.off()
 
 ## calculate overlap
-classifier <- nmf.output.2@fit@H
+classifier <- nmf.output.glioma.4@fit@H
 classifier <- as.data.frame(t(classifier))
-classifier$subtype <- column.annotations$Cancer_Type_Broaddd
-classifier$group <- "A"
-classifier$group[classifier$V2 > classifier$V1] <- "B"
-pdf("NMF K2 summary.pdf")
-ggplot(data = classifier, aes(x = subtype, fill = group)) + geom_bar(position = "dodge")
+classifier$subtype <- column.annotations$Cancer_Type_Glioma_Grade
+classifier$basis <- sapply(1:nrow(classifier), function(x){which(classifier[x, 1:(ncol(classifier) - 1)] == max(classifier[x, 1:(ncol(classifier) -1)]))[1]}, 
+                           USE.NAMES = FALSE)
+classifier$subtype[classifier$subtype == ""] <- "unlabeled"
+
+## percentage based classifier
+percent.classifier <- table(classifier$basis, classifier$subtype)
+for (i in 1:ncol(percent.classifier)){
+    percent.classifier[, i] <- percent.classifier[, i] / sum(percent.classifier[, i])
+}
+percent.classifier <- as.data.frame(percent.classifier)
+colnames(percent.classifier) <- c("basis", "subtype", "percent")
+percent.classifier$basis <- paste("factor", percent.classifier$basis, sep = "_")
+
+
+pdf("NMF Glioma K4 summary by basis.pdf", width = 10, height = 7)
+colors <- distinctColorPalette(length(unique(classifier$subtype)))
+names(colors) <- unique(classifier$subtype)
+ggplot(data = classifier, aes(x = basis, fill = factor(subtype))) + geom_bar(position = "dodge") + scale_fill_manual(values = colors)
 dev.off()
 
+pdf("NMF Glioma K4 summary by basis percent.pdf", width = 10, height = 7)
+ggplot(data = percent.classifier, aes(x = basis, y = percent, fill = subtype)) + geom_bar(stat = "Identity", position = "dodge") + scale_fill_manual(values = colors)
+dev.off()
+
+
+
+pdf("NMF Glioma K4 summary by subtype with grade.pdf", width = 10, height = 7)
+ggplot(data = classifier, aes(x = subtype, fill = factor(basis))) + geom_bar(position = "dodge") + theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
++ coord_flip()
+dev.off()
+
+pdf("NMF Glioma K4 summary by subtype percent.pdf", width = 10, height = 7)
+ggplot(data = percent.classifier, aes(x = subtype, y = percent, fill = basis)) + geom_bar(stat = "Identity", position = "dodge") + theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+dev.off()
+
+## plot percentage of samples within a given cancer type residing in most dominant factor
+max.values <- data.frame(sapply(as.character(unique(percent.classifier$subtype)), function(x){max(percent.classifier$percent[percent.classifier$subtype == x])}))
+max.values$subtype <- rownames(max.values)
+colnames(max.values) <- c("percent", "subtype")
+pdf("NMF CNVs K4 summary max basis.pdf", width = 10, height = 7)
+ggplot(data = max.values, aes(x = subtype, y = percent, fill = subtype)) + geom_bar(stat = "identity") + scale_fill_manual(values = colors) + 
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) 
+dev.off()
+
+
+## plot contributing factors
+tiff("../Analysis/NMF CNVs K4 components.tiff", width = 1000, height = 1000)
+basismap(nmf.output.cnvs.4)
+dev.off()
 ## show relationship between input variables and metagenes, with each variable coded by the dominant metagene it contributes to
-basismap(output)
 
 
 ## show a consensus map of the clustering based on multiple iterations
